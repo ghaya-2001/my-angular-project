0a68e6e2342984e6d7597d8b66b011d4a090e855
Ghaya Derbali <ghayaderbali20@gmail.com>
2023-03-09 16:53:16 +0100

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { IntroComponent } from './intro/intro.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
